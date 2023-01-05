3ea6f6b6bb1a11fd9adee370b95bd838365531ef
Enzo VALENTIN <Valentinenzo@outlook.fr>
2023-01-05 16:29:03 +0100

    tp02 fini

--- FRONTEND/tp02/src/app/app.module.ts
+++ FRONTEND/tp02/src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TetiereComponent } from './tetiere/tetiere.component';
+import { FooterComponent } from './footer/footer.component';
+import { FormulaireSaisieClientComponent } from './formulaire-saisie-client/formulaire-saisie-client.component';
+import { RecapSaisieClientComponent } from './recap-saisie-client/recap-saisie-client.component';
+import { ErrorDirective } from './error.directive';
+import { PhoneNumberPipe } from './phone-number.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TetiereComponent,
+    FooterComponent,
+    FormulaireSaisieClientComponent,
+    RecapSaisieClientComponent,
+    ErrorDirective,
+    PhoneNumberPipe,
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
